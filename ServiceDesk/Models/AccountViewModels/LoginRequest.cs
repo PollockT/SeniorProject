2812033e2e0402e771a09f544389337b0e685451
Theodore Pollock <PollockT@TulsaPretech.com>
2020-10-06 20:04:09 -0500

    Building Views

---
+++ ServiceDesk/Models/AccountViewModels/LoginRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ServiceDesk.Models.AccountViewModels
+{
+    public class LoginRequest
+    {   
+        /// <summary>
+        /// Gets User Name
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        /// Gets User Password
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Ability to allow user's info to be stored
+        /// on login
+        /// </summary>
+        public bool Cached { get; set; }
+    }
+}
---
+++ ServiceDesk/Models/ReportsViewModels/ReportDetails.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceDesk.Models.ReportsViewModels
+{
+    public class ReportDetails
+    {
+    }
+}
